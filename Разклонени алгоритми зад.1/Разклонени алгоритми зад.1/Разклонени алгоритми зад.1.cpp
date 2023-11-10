c14309c0ab747d09e8feb1d46c43aa77d231e32d
AGMVarna <99265210+AGMVarna@users.noreply.github.com>
2023-11-10 20:46:05 +0200

    Add files via upload

---
+++ Разклонени алгоритми зад.1/Разклонени алгоритми зад.1/Разклонени алгоритми зад.1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+
+int num;
+
+int main() {
+	system("chcp 1251");
+	cout << "Въведете цяло число: "; cin >> num;
+	if (num % 2 == 0)
+		cout << "Четно\n";
+	else cout << "Нечетно\n";
+	system("pause");
+	return 0;
+}
+/* Напишете програма, която приема от клавиатурата цяло число и отпечатва "Yes", ако е четно и "No", ако е нечетно. */
